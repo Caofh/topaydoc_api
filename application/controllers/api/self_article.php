65de4e799d6a0b4cf25d9e00423879b8fbadc7d1
fanghui.caoNew <fanghui.cao@mfashion.com.cn>
2017-10-01 23:28:33 +0800

    update-first-version.

---
+++ application/controllers/api/self_article.php
@@ -0,0 +1,31 @@
+<?php
+class Self_article extends CI_Controller {
+
+    function __construct()
+    {
+        parent::__construct();
+    }
+
+
+    public function index(){
+
+//        var_dump(111);
+//        exit;
+
+        $this->load->model('content');
+        $query = $this->content->get_last_ten_entries();
+
+        $data = [
+          'data' => $query
+        ];
+
+        $out_data = out_format($data, '请求成功');
+
+        renderJson($out_data);
+
+    }
+
+
+
+}
+?>
